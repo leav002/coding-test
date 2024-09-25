f44d04826e8711313fae3406fa10a51e97433423
leav002 <165896203+leav002@users.noreply.github.com>
2024-09-25 09:05:57 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.05 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        
+        int answer = 1;
+        int answer1 = -1;
+        if(num1==num2){
+            return answer;
+        }else if(num1 != num2){
+            return answer1;
+        }
+        return answer;
+    }
+}
