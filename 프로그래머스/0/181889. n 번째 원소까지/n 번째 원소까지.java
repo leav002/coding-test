5e5ed1639b8cded946774e43048c8fd5f7bfd6ba
leav002 <165896203+leav002@users.noreply.github.com>
2024-12-15 21:13:09 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int ans_list[] = new int[n];
+        for(int i = 0; i < n; i++){
+            ans_list[i] = num_list[i];
+        }
+        return ans_list;
+    }
+}
