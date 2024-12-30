9bf8172e8f1c5393d2f2ce673c02ff5502da5236
leav002 <165896203+leav002@users.noreply.github.com>
2024-12-30 09:31:37 +0900

    [Bronze V] Title: 시험 성적, Time: 180 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        n = n/10;
+
+        switch(n){
+            case 10 : case 9: System.out.println("A");break;
+            case 8: System.out.println("B");break;
+            case 7: System.out.println("C");break;
+            case 6: System.out.println("D");break;
+            default : System.out.println("F");break;
+        }
+    }
+}
