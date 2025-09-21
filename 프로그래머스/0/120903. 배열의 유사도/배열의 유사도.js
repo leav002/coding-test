e958b1d724a223fa0c9198f761f016ae3f503f35
leav002 <165896203+leav002@users.noreply.github.com>
2025-09-22 08:44:27 +0900

    [level 0] Title: 배열의 유사도, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -0,0 +1,12 @@
+function solution(s1, s2) {
+    var answer = 0;
+    
+    for (let i of s1) {
+        for(let j of s2) {
+            if (i == j) {
+                answer += 1;
+            }
+        }
+    }
+    return answer;
+}
